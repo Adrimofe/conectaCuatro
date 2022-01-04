d2bd24a58112e5017afc694c586e6ed408f1b6b2
ClosedPixel <adrianmorenofdez@gmail.com>
2022-01-04 15:07:54 +0100

    actualización datanum de 0 a 41 y condiciones if

--- script/programa.js
+++ script/programa.js
@@ -5,3 +5,59 @@ Al seleccionar la columna se coloca la ficha en la parte inferior.
 Guardar el número partidas ganadas de cada equipo mientras la App no se finaliza.
 Dos botones para reiniciar la partida en marcha y para resetear el contador de victorias y derrotas.
 Puedes añadirle todas las funcionalidades extra que consideres. */
+
+const fichas = document.getElementsByClassName('tablero__casilla');
+const rojo = "background-color: red";
+const amarillo = "background-color: yellow";
+const blanco = "background-color: white";
+let acumulador = 0;
+const conjuntoFichas = [];
+class ficha {
+    constructor(numero) {
+        this._numero = numero;
+    }
+    _color = blanco;
+    get color() {
+        return this._color;
+    }
+    set color(value) {
+        this._color = value;
+    }
+    _numero = 0;
+    get numero() {
+        return this._numero;
+    }
+    set numero(value) {
+        this._numero = value;
+    }
+    _activo = false;
+    get activo() {
+        return this._activo;
+    }
+    set activo(value) {
+        this._activo = value;
+    }
+}
+
+for (let index = 1; index < 43; index++) {
+    conjuntoFichas.push(new ficha(index));
+}
+
+for (let index = 0; index < fichas.length; index++) {
+    const element = fichas[index];
+    element.addEventListener('click', (e) => {
+        if (e.target.dataset.casillaNum && acumulador % 2 == 0 && !conjuntoFichas[e.target.dataset.casillaNum].activo) {
+            acumulador++;
+            e.target.style.cssText = rojo;
+            conjuntoFichas[e.target.dataset.casillaNum].activo = true;  
+
+        } else if (e.target.dataset.casillaNum && !acumulador % 2 == 0  && !conjuntoFichas[e.target.dataset.casillaNum].activo) {
+            acumulador++;
+            e.target.style.cssText = amarillo;
+            conjuntoFichas[e.target.dataset.casillaNum].activo = true;    
+        }
+    })
+
+    
+}
+
