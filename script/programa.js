7572fc3b615873aafd04e319ea7e2caa8f95c868
ClosedPixel <adrianmorenofdez@gmail.com>
2022-01-04 11:38:44 +0100

    Creación DIVS 6x7 y responsive design

---
+++ script/programa.js
@@ -0,0 +1,7 @@
+/**Tablero de 7x6 (7 en el eje "x" y 6 en el "y").
+Fichas Rojas y Amarillas. La primera partida la comienza siempre la Roja (la segunda la Amarilla, la tercera la Roja...).
+No hay que implementar una funcionalidad que te permita jugar contra la App. Se asume que jugarán dos personas reales alternándose.
+Al seleccionar la columna se coloca la ficha en la parte inferior.
+Guardar el número partidas ganadas de cada equipo mientras la App no se finaliza.
+Dos botones para reiniciar la partida en marcha y para resetear el contador de victorias y derrotas.
+Puedes añadirle todas las funcionalidades extra que consideres. */
